8e2ad53c017e7bbe02140404c17477367c378dc8
dqqzj <798078824@qq.com>
2018-05-13 10:08:10 +0800

    fastfds

---
+++ fastfds/src/main/java/com/qzj/fastfds/FastfdsApplication.java
@@ -0,0 +1,12 @@
+package com.qzj.fastfds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastfdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastfdsApplication.class, args);
+    }
+}
